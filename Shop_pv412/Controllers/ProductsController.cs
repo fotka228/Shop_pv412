4768a84a7fcdb14862d6ee1e3c8191eb0fdd72e6
fotka228 <koilinskij@gmail.com>
2026-01-27 20:47:34 +0200

    Some Updates

--- Shop_pv412/Controllers/ProductsController.cs
+++ Shop_pv412/Controllers/ProductsController.cs
@@ -2,11 +2,31 @@
 
 namespace Shop_pv412.Controllers
 {
-    public class ProductsController : Controller
+    public  class ProductsController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public  async Task<IActionResult> ReadProducts()
         {
-            return View();
+            //Get products from database
+            return View(/*products*/);
         }
+        [HttpGet]
+        public IActionResult CreateProduct() => View();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProduct([Bind("Name,Price,Description")]Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                //add product to database
+            }
+            else
+            {
+                //return error 
+            }
+            return RedirectToAction("ReadProducts","Products");
+
+        }
+
     }
 }
